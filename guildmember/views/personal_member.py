fb926e43948a55098594becd588aec093c328b61
bell <bellv@prontomarketing.com>
2017-10-01 20:55:26 +0700

    Add member detail to template

--- guildmember/views/personal_member.py
+++ guildmember/views/personal_member.py
@@ -1,19 +1,38 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from ..models import Member
+from ..models import Member, Job, Friend
 
 
 class PersonalMemberView(TemplateView):
     template = 'personal_member.html'
 
+    def retrive_member_data(self, member_id):
+        member = Member.objects.get(id=member_id)
+        try:
+            character = Job.objects.get(name=member.gb_class)
+        except:
+            character = None
+
+        try:
+            waifu = Friend.objects.get(name=member.gb_waifu)
+        except:
+            waifu = None
+
+        return member, character, waifu
+
     def get(self, request, **kwargs):
-        members = Member.objects.all().filter(status='active')
+        member, character, waifu = self.retrive_member_data(
+            self.kwargs['member_id']
+        )
 
         return render(
             request,
             self.template,
             {
-                'members': members
+                'member': member,
+                'character': character,
+                'waifu': waifu,
+                'message': '<img src="https://gbf.wiki/images/2/27/Fighter_djeeta_icon.jpg">'
             }
         )
