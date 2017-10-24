e4de57e75804594fbe2f9d5753ad502195b7d78b
bell <bellv@prontomarketing.com>
2017-10-24 08:26:10 +0700

    Add serializers.py prototype

---
+++ guildmember/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+from posts.models import Post
+from .serializers import PostSerializer
+
+class PostListAPIView(ListAPIView):
+    queryset = Post.objects.all()
